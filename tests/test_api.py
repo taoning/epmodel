5af36ddd5174b5601f99a2b893ba26cc369d53f0
Taoning Wang <taoning@gmail.com>
2023-09-12 09:44:54 -0500

    test: add a test that validates a component

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import json
-from epmodel import EnergyPlusModel
+from epmodel import (
+    EnergyPlusModel,
+    WindowMaterialGas,
+    GasType,
+)
 import pytest
 
 @pytest.fixture
@@ -27,3 +31,13 @@ def test_validate2(test_file2):
         json_data = json.load(f)
     model = EnergyPlusModel.model_validate(json_data)
     assert model.version is not None
+
+def test_validate_component():
+    gas = WindowMaterialGas(
+        gas_type=GasType.air,
+        thickness=0.2,
+        molecular_weight=28.97,
+    )
+    gas.thickness = -0.3
+    with pytest.raises(Exception) as e_info:
+        WindowMaterialGas.model_validate(gas.__dict__)
