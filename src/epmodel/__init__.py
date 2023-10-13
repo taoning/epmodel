349beb47f9e1a0ec1e54d5fb1cf9199bac29bed8
Taoning Wang <taoning@gmail.com>
2023-10-13 16:24:07 -0700

    change top level api to limit to builder, individual component has to be accessed by from epmodel import epmodel

--- src/epmodel/__init__.py
+++ src/epmodel/__init__.py
@@ -1,13 +1,3 @@
-import inspect
-import importlib
-
-# Import all classes from module_a
-epmodel = importlib.import_module('.epmodel', 'epmodel')
-for name, obj in inspect.getmembers(epmodel):
-    if inspect.isclass(obj) and obj.__module__ == epmodel.__name__:
-        globals()[name] = obj
-
-
 from .builder import (
     ConstructionComplexFenestrationStateInput,
     ConstructionComplexFenestrationStateLayerInput,
@@ -15,3 +5,11 @@ from .builder import (
     ConstructionComplexFenestrationStateBuilder,
     EnergyPlusModel,
 )
+
+__all__ = [
+    "ConstructionComplexFenestrationStateInput",
+    "ConstructionComplexFenestrationStateLayerInput",
+    "ConstructionComplexFenestrationStateGapInput",
+    "ConstructionComplexFenestrationStateBuilder",
+    "EnergyPlusModel",
+]
